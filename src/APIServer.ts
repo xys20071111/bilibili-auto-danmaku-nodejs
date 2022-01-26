0d731d4746c923e71fb0d5eb99c5e067cc92b3a1
xys20071111 <xys20071111@hotmail.com>
2022-01-26 17:27:11 +0800

    修复bug

--- src/APIServer.ts
+++ src/APIServer.ts
@@ -2,18 +2,22 @@ import { Server, WebSocket } from "ws"
 import { EventEmitter } from 'events'
 import config from './config'
 import sendDanmake from "./sendDanmaku"
+import { startPlugins } from "./startPlugins";
 
 interface Message {
   cmd: string
   data: any
 }
 
-const AuthedScoketSet = new Set<WebSocket>()
-const APIMsgHandler = new EventEmitter()
+const AuthedScoketSet = new Set<WebSocket>();
+const APIMsgHandler = new EventEmitter();
 
 APIMsgHandler.on('AUTH', (socket: WebSocket, data: string) => {
   if(data === config.api.token && !AuthedScoketSet.has(socket)) {
     AuthedScoketSet.add(socket)
+    socket.send(JSON.stringify({cmd: 'AUTH', data: 'AUTHED'}));
+  }else{
+    socket.send(JSON.stringify({cmd: 'AUTH', data: 'FAILED'}));
   }
 })
 
@@ -21,9 +25,18 @@ APIMsgHandler.on('SEND', (socket, data: string) => {
   if(AuthedScoketSet.has(socket)) {
     sendDanmake({
       msg: data
-    })
+    });
   }
-})
+});
+
+APIMsgHandler.on('ROOMID', (socket: WebSocket) => {
+  if(AuthedScoketSet.has(socket)) {
+    socket.send(JSON.stringify({
+      msg: 'ROOMID',
+      data: config.room_id
+    }));
+  }
+});
 
 const wsServer = new Server({
   port: config.api.port
@@ -32,18 +45,19 @@ const wsServer = new Server({
 wsServer.on('connection', (socket: WebSocket) => {
   socket.on('message', (rawData: string) => {
     try{
-      const msg: Message = JSON.parse(rawData)
-      APIMsgHandler.emit(msg.cmd, socket, msg.data)
+      const msg: Message = JSON.parse(rawData);
+      APIMsgHandler.emit(msg.cmd, socket, msg.data);
     } catch(e) {
-      console.log(e)
+      console.log(e);
     }
-  })
+  });
 
   socket.on('close', () => {
-    AuthedScoketSet.delete(socket)
-  })
-})
+    AuthedScoketSet.delete(socket);
+  });
+});
 
+startPlugins();
 
 export default AuthedScoketSet
-export {AuthedScoketSet, APIMsgHandler, wsServer }
+export { AuthedScoketSet, APIMsgHandler, wsServer }
