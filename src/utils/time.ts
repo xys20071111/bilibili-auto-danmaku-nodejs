0cacaff0396a391264930ca0083d5e9996d5ff5d
xys20071111 <xys20071111@hotmail.com>
2022-01-31 10:06:01 +0800

    修复获取时间字符串函数的格式错误问题

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -4,9 +4,10 @@ function getTimeString(): string {
   const month = time.getMonth() + 1
   const day = time.getDate()
   const hour = time.getHours()
+  const minutes = time.getMinutes()
   const second = time.getSeconds()
 
-  return `${year}-${month}-${day}-${hour}-${second}`
+  return `${year}-${month}-${day}-${hour}-${minutes}-${second}`
 }
 
 export { getTimeString }
