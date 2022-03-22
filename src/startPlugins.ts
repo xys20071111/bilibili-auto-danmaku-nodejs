f6a551cc6a60a1d48e503c630ad6fdd3d3eeaed7
xys20071111 <xys20071111@hotmail.com>
2022-03-22 21:26:28 +0800

    更改启动插件的配置

--- src/startPlugins.ts
+++ src/startPlugins.ts
@@ -7,7 +7,7 @@ export function startPlugins() {
     if(fs.statSync(`./plugins/${val}`).isDirectory()) {
       const jsonStr = fs.readFileSync(`./plugins/${val}/package.json`, {encoding: 'utf8'})
       const packageJson = JSON.parse(jsonStr)
-      cp.fork(`./plugins/${val}/${packageJson.main}`)
+      cp.fork(`./plugins/${val}/${packageJson.main}`, ['./plugins/${val}/config.json'])
     }
   });
 }
